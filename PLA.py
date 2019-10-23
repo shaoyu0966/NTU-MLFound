2e13b9abc821f6c0d5a5bf9825b0bd210df0fdf9
shaoyu0966 <b06705028@ntu.edu.tw>
2019-10-23 15:18:26 +0800

    algorithm implemented

--- PLA.py
+++ PLA.py
@@ -5,6 +5,7 @@
 # histogram
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 class PLA:
 
@@ -14,8 +15,30 @@ class PLA:
         self.x = np.insert(data[:, 0:-1], 0, 1, axis=1)
         self.y = data[:, -1]
         f.close()
+    
+    def init(self):
+        self.w = np.zeros(np.shape(self.x)[1])
+        self.seq = np.arange(np.shape(self.x)[0])
+        self.random_seq(10)
+    
+    def random_seq(self, seed):
+        np.random.seed(seed)
+        np.random.shuffle(self.seq)
+    
+    def run(self):
+        prev = np.shape(self.x)[0] - 1
+        curr = 0
+        while not curr == prev:
+            if not (np.inner(self.w, self.x[self.seq[curr]]) > 0) == (self.y[self.seq[curr]] > 0):
+                self.w = self.w + self.y[self.seq[curr]] * self.x[self.seq[curr]]
+                prev = curr
+            curr = (curr + 1) % np.shape(self.x)[0]
+        print(self.w)
+
 
 pla = PLA()
-pla.load_data('hw1_6_train.dat')
+pla.load_data('testdata.dat')
+pla.init()
+pla.run()
 
 
