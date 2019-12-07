788ff0c3e647d21e34ea2ebcc56a3bb658ab9de5
shaoyu0966 <b06705028@ntu.edu.tw>
2019-12-07 17:16:40 +0800

    remove files

--- test_generator.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-f = open('testdata.dat', 'w')
-for i in range(10000):
-    x1, x2 = np.random.random() - 0.5, np.random.random() - 0.5
-    y = 1 if x1 + x2 > 0 else -1
-    f.write(f'{x1} {x2} {y}\n')
-f.close()
