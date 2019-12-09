926d34009a27d4b79facc1e3949381fda7933451
shaoyu0966 <b06705028@ntu.edu.tw>
2019-12-09 21:09:17 +0800

    dicision stump algorithm

--- hw2/dicision_stump.py
+++ hw2/dicision_stump.py
@@ -9,10 +9,29 @@ class DicisionStump:
     def generateData(self):
         self.x = np.random.rand(self.size)
         self.x = self.x * 2 - 1
-        self.y = np.sign(self.x)
-        self.y = list(map(lambda y_i: y_i if np.random.random_sample() > self.noise else -y_i, self.y))
+        self.y = list(map(lambda y_i: 1 if y_i > 0 else -1, self.x))
+        self.y = np.array(list(map(lambda y_i: y_i if np.random.random_sample() > self.noise else -y_i, self.y)))
     
+    def Ein(self, theta, s):
+        result = np.array(list(map(lambda larger: 1 if larger else -1, self.x > theta))) * s
+        correct = (result == self.y)
+        return (self.size - correct.sum())
+
+    def dicisionStump(self):
+        theta_set = np.append(self.x, -1)
+        s_set = [1, -1]
+        self.best_theta_s = (theta_set[0], s_set[0])
+        self.best_Ein = self.size
+        for theta in theta_set:
+            for s in s_set:
+                ein = self.Ein(theta, s)
+                if ein < self.best_Ein:
+                    self.best_theta_s = (theta, s)
+                    self.best_Ein = ein
+
+                
         
 ds = DicisionStump()
 ds.init(20, 0.2)
 ds.generateData()
+ds.dicisionStump()
