53d3edc3a36010b25c596588e4ce9292f8d146cf
shaoyu0966 <b06705028@ntu.edu.tw>
2019-12-09 21:20:15 +0800

    Eout for dicision stump, experiment

--- hw2/dicision_stump.py
+++ hw2/dicision_stump.py
@@ -15,7 +15,11 @@ class DicisionStump:
     def Ein(self, theta, s):
         result = np.array(list(map(lambda larger: 1 if larger else -1, self.x > theta))) * s
         correct = (result == self.y)
-        return (self.size - correct.sum())
+        error = self.size - correct.sum()
+        return error/self.size
+    
+    def Eout(self, theta, s):
+        return 0.5 + 0.3 * s * (abs(theta) - 1)
 
     def dicisionStump(self):
         theta_set = np.append(self.x, -1)
@@ -24,14 +28,26 @@ class DicisionStump:
         self.best_Ein = self.size
         for theta in theta_set:
             for s in s_set:
-                ein = self.Ein(theta, s)
-                if ein < self.best_Ein:
+                e_in = self.Ein(theta, s)
+                if e_in < self.best_Ein:
                     self.best_theta_s = (theta, s)
-                    self.best_Ein = ein
+                    self.best_Ein = e_in
+        self.best_Eout = self.Eout(self.best_theta_s[0], self.best_theta_s[1])
 
-                
-        
-ds = DicisionStump()
-ds.init(20, 0.2)
-ds.generateData()
-ds.dicisionStump()
+# ds = DicisionStump()
+# ds.init(20, 0.2)
+# ds.generateData()
+# ds.dicisionStump()
+
+Ein = []
+Eout = []
+for i in range(5000):
+    ds = DicisionStump()
+    ds.init(20, 0.2, seed=i)
+    ds.generateData()
+    ds.dicisionStump()
+    Ein.append(ds.best_Ein)
+    Eout.append(ds.best_Eout)
+
+print(np.average(Ein))
+print(np.average(Eout))
