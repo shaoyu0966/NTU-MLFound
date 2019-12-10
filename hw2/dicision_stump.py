9a2d27015292e691755e373ab0d1946c5a692e5e
shaoyu0966 <b06705028@ntu.edu.tw>
2019-12-10 14:38:25 +0800

    multi-dimension dicision stump

--- hw2/dicision_stump.py
+++ hw2/dicision_stump.py
@@ -2,12 +2,16 @@ import numpy as np
 
 class DicisionStump:
 
-    def init(self, size, noise, seed=0):
-        self.size = size
-        self.noise = noise
+    def loadData(self, x, y):
+        self.x = x
+        self.y = y
+        self.dim = np.shape(y)[0]
 
-    def generateData(self):
-        self.x = np.random.rand(self.size)
+    def generateData(self, dim, noise, seed=0):
+        np.random.seed(seed)
+        self.dim = dim
+        self.noise = noise
+        self.x = np.random.rand(self.dim)
         self.x = self.x * 2 - 1
         self.y = list(map(lambda y_i: 1 if y_i > 0 else -1, self.x))
         self.y = np.array(list(map(lambda y_i: y_i if np.random.random_sample() > self.noise else -y_i, self.y)))
@@ -15,8 +19,8 @@ class DicisionStump:
     def Ein(self, theta, s):
         result = np.array(list(map(lambda larger: 1 if larger else -1, self.x > theta))) * s
         correct = (result == self.y)
-        error = self.size - correct.sum()
-        return error/self.size
+        error = self.dim - correct.sum()
+        return error / self.dim
     
     def Eout(self, theta, s):
         return 0.5 + 0.3 * s * (abs(theta) - 1)
@@ -25,7 +29,7 @@ class DicisionStump:
         theta_set = np.append(self.x, -1)
         s_set = [1, -1]
         self.best_theta_s = (theta_set[0], s_set[0])
-        self.best_Ein = self.size
+        self.best_Ein = self.dim
         for theta in theta_set:
             for s in s_set:
                 e_in = self.Ein(theta, s)
@@ -34,20 +38,63 @@ class DicisionStump:
                     self.best_Ein = e_in
         self.best_Eout = self.Eout(self.best_theta_s[0], self.best_theta_s[1])
 
+
+class MultiDimDicisionStump:
+
+    def loadData(self, file_name):
+        f = open(file_name, 'r')
+        data = np.genfromtxt(file_name)
+        self.x = data[:, 0:-1]
+        self.y = data[:, -1]
+        f.close()
+
+    def dicisionStump(self):
+        self.best_dim = None
+        self.best_theta_s = None
+        self.best_Ein = np.shape(self.y)[0]
+        for i in range(np.shape(self.x)[1]):
+            ds = DicisionStump()
+            ds.loadData(self.x[:, i], self.y)
+            ds.dicisionStump()
+            if ds.best_Ein < self.best_Ein:
+                self.best_dim = i
+                self.best_theta_s = ds.best_theta_s
+                self.best_Ein = ds.best_Ein
+    
+    def testing(self, test_file):
+        f = open(test_file, 'r')
+        data = np.genfromtxt(test_file)
+        self.test_x = data[:, 0:-1]
+        self.test_y = data[:, -1]
+        f.close()
+
+        x = self.test_x[:, self.best_dim]
+        theta = self.best_theta_s[0]
+        s = self.best_theta_s[1]
+        y_predict = np.array(list(map(lambda larger: 1 if larger else -1, x > theta))) * s
+        err = (self.test_y != y_predict).sum()
+        return err / np.shape(self.test_y)
+
+
+mdds = MultiDimDicisionStump()
+mdds.loadData('multi_dim_ds_train.dat')
+mdds.dicisionStump()
+print(mdds.testing('multi_dim_ds_test.dat'))
+
+
+
 # ds = DicisionStump()
-# ds.init(20, 0.2)
-# ds.generateData()
+# ds.generateData(20, 0.2)
 # ds.dicisionStump()
 
-Ein = []
-Eout = []
-for i in range(5000):
-    ds = DicisionStump()
-    ds.init(20, 0.2, seed=i)
-    ds.generateData()
-    ds.dicisionStump()
-    Ein.append(ds.best_Ein)
-    Eout.append(ds.best_Eout)
+# Ein = []
+# Eout = []
+# for i in range(5000):
+#     ds = DicisionStump()
+#     ds.generateData(20, 0.2, seed=i)
+#     ds.dicisionStump()
+#     Ein.append(ds.best_Ein)
+#     Eout.append(ds.best_Eout)
 
-print(np.average(Ein))
-print(np.average(Eout))
+# print(np.average(Ein))
+# print(np.average(Eout))
