f0351ab6f3038ca3354acefc4aba2996b9b0904a
shaoyu0966 <b06705028@ntu.edu.tw>
2020-01-13 17:01:32 +0800

    add main_8

--- hw3/gradient_decent.py
+++ hw3/gradient_decent.py
@@ -10,23 +10,41 @@ class GradientDecent:
         E = sp.lambdify([self.u, self.v], self.E_ex, 'math')
         return E(u, v)
 
-    def gradient(self):
-        self.dE_du = sp.lambdify([self.u, self.v], sp.diff(self.E_ex, self.u), 'math')
-        self.dE_dv = sp.lambdify([self.u, self.v], sp.diff(self.E_ex, self.v), 'math')
+    def gradient(self, u=0, v=0):
+        self.dE_du_ex = sp.diff(self.E_ex, self.u)
+        self.dE_dv_ex = sp.diff(self.E_ex, self.v)
+        self.dE_du = sp.lambdify([self.u, self.v], self.dE_du_ex, 'math')
+        self.dE_dv = sp.lambdify([self.u, self.v], self.dE_dv_ex, 'math')
+        return self.dE_du(u, v), self.dE_dv(u, v)
     
+    def second_order_derivative(self, u=0, v=0):
+        self.d2E_duu_ex = sp.diff(self.dE_du_ex, self.u)
+        self.d2E_dvv_ex = sp.diff(self.dE_dv_ex, self.v)
+        self.d2E_duv_ex = sp.diff(self.dE_du_ex, self.v)
+        self.d2E_duu = sp.lambdify([self.u, self.v], self.d2E_duu_ex, 'math')
+        self.d2E_dvv = sp.lambdify([self.u, self.v], self.d2E_dvv_ex, 'math')
+        self.d2E_duv = sp.lambdify([self.u, self.v], self.d2E_duv_ex, 'math')
+        return self.d2E_duu(u, v),  self.d2E_dvv(u, v), self.d2E_duv(u, v)
+
     def improve(self, step_size, u0, v0, n_iter):
         cur_u, cur_v = u0, v0
         for i in range(n_iter):
-            print(i, cur_u, cur_v)
-            u = cur_u - step_size * self.dE_du(cur_u, cur_v)
-            v = cur_v - step_size * self.dE_dv(cur_u, cur_v)
-            cur_u = u
-            cur_v = v
+            cur_u, cur_v = cur_u - step_size * self.dE_du(cur_u, cur_v), cur_v - step_size * self.dE_dv(cur_u, cur_v)
         return cur_u, cur_v
 
-if __name__ == '__main__':
+def main_7():
     gd = GradientDecent()
     gd.functionE()
     gd.gradient()
-    u5, v5 = gd.improve(0.01, 0.0, 0.0, 5)
-    print(gd.functionE(u5, v5))
+    
+
+def main_8():
+    u, v = 0, 0
+    gd = GradientDecent()
+    b = gd.functionE(u, v)
+    b_u, b_v = gd.gradient(u, v)
+    b_uu, b_vv, b_uv = gd.second_order_derivative(u, v)
+    print(b_uu/2, b_vv/2, b_uv, b_u, b_v, b)
+
+if __name__ == "__main__":
+    main_8()
